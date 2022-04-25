7387781212d4e9f9d75c1e6948b3c1d346a33815
Natanael Bezerra <nsbbezerra@hotmail.com>
2022-04-25 08:13:14 -0300

    change port

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { app } from "./app";
 
-const PORT = process.env.PORT || 4003;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, function () {
   console.log(`API running on port: ${PORT}`);
